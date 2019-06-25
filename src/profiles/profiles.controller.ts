ae7dd8ee8a8074c3b27d51369bc531c1d4efb3c3
ThiagoRAD <thauroo@gmail.com>
2019-06-25 15:57:04 -0300

    find by id

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('profiles')
 export class ProfilesController {
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('age') age: number) {
+    return [{ age }];
+  }
+  @Get(':id')
+  findProfileById(@Param('id') id: string) {
+    return { id, name: 'John Doe' };
   }
 }
