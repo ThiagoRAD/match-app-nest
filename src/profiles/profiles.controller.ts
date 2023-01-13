64d37e15d6b623ac540b6e290b387248c0fcc264
ThiagoRAD <thauroo@gmail.com>
2023-01-13 15:47:36 -0300

    api.http

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  @Get()
+  findAll() {
+    return [];
+  }
+}
